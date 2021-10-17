276d99e4be1deefd9b81eda7c1bc738218291344
Nasian <87439662+nasiana@users.noreply.github.com>
2021-10-17 22:48:12 +0100

    Add files via upload

--- Lecture.py
+++ Lecture.py
@@ -51,4 +51,96 @@ print(response.status_code)
 data = response.json()
 pp(data)
 
-# DO EXERCISE 3 TOMORROW WHEN I'M HOME
+# EXERCISE 3
+
+import requests
+from datetime import datetime
+from pprint import pprint as pp
+
+# this endpoint tells the current location for international space station
+endpoint3 = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(endpoint3)
+
+data = response.json()
+pp(data)
+
+timestamp = data['timestamp']
+dt_object = datetime.fromtimestamp(timestamp)
+
+print("dt_object =", dt_object)
+print("type(dt_object) =", type(dt_object))
+
+msg = "At {dt} the ISS was passing the following location, latitude: {lat} and longitude: {lon}".format(
+    dt = dt_object,
+    lat = data['iss_position']['latitude'],
+    lon = data['iss_position']['longitude'],
+    our_msg = data['message']
+)
+print(msg)
+
+with open('space_station_location.txt', 'a') as text_file:
+    text_file.write(msg + '\n')
+
+# EXERCISE 4
+
+import requests
+from pprint import pprint as pp
+
+appid = 'Tester2021'  # key to connect to the API --> create a free account and paste your OWN key here
+
+endpoint = 'http://api.openweathermap.org/data/2.5/weather' # see doc to customise your payload
+
+payload = {
+    'q': 'London,UK',
+    'unit': 'metrics',
+    'appid': appid,
+}
+
+response = requests.get(url=endpoint, params=payload)
+
+data = response.json()
+
+
+pp(data['name'])
+pp(data['weather'])
+
+pp(data)
+
+# EXERCISE 5
+
+import requests
+from pprint import pprint
+
+pokemon_number = input("What is the Pokemon's ID? ")
+
+url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number) # note how we manupulate the url to request data!
+
+response = requests.get(url)
+print(response)
+
+pokemon = response.json()
+pprint(pokemon)
+
+# EXERCISE 6
+
+import requests
+from pprint import pprint
+
+pokemon_number = input("What is the Pokemon's ID? ")
+
+url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+
+response = requests.get(url)
+pokemon = response.json()
+
+pprint(pokemon)
+# PRINT THE NAME HERE
+# PRINT THE HEIGHT HERE
+# PRINT THE WEIGHT HERE
+
+# use a for loop to iterate over the moves. Try to store all moves in a variable and see what it looks like.   
+for i in pokemon['moves']:
+    print(i['move']['name'])
+    print(i['move']['height'])
+    print(i['move']['weight'])
